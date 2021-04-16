120d6e3bd510f9f34cc161b9ecb2dbde8cae1488
bilaz666 <victor.me.marc@gmail.com>
2021-04-16 17:29:22 +0200

    commit

---
+++ server.js
@@ -0,0 +1,75 @@
+const express = require('express')
+const app = express()
+
+const PORT = 3333
+const IP_LOOPBACK = 'localhost'
+//Message a la racine
+app.get('/', (req, res) => {
+  res.send('Welcome to Calculator, please go to "/calc"')
+})
+//Message d'usage dans "/calc"
+app.get('/calc', (req, res) => {
+  res.send('Operators you can use: add, sub, mul, div, mod.')
+})
+//Calculatrice
+app.get('/calc/:operator/:op1/:op2', (req, res) => {
+  const operator = req.params.operator
+  const op1 = req.params.op1
+  const op2 = req.params.op2
+
+  //check que les deux input soit bien des nombres
+  if (isNaN(op1) || isNaN(op2)) {
+    res.send('Please use a number')
+  } else {
+
+    switch (operator) {
+      case 'add':
+        try {
+          const resultAdd = { op: 'add', op1: Number(op1), op2: Number(op2), result: Number(op1) + Number(op2) }
+          res.json(resultAdd)
+        } catch (e) {
+          res.send(e.message)
+        }
+        break
+      case 'sub':
+        try {
+          const resultSub = { op: 'sub', op1: Number(op1), op2: Number(op2), result: Number(op1) - Number(op2) }
+          res.json(resultSub)
+        } catch (e) {
+          res.send(e.message)
+        }
+        break
+      case 'mul':
+        try {
+          const resultMul = { op: 'mul', op1: Number(op1), op2: Number(op2), result: Number(op1) * Number(op2) }
+          res.json(resultMul)
+        } catch (e) {
+          res.send(e.message)
+        }
+        break
+      case 'div':
+        try {
+          const resultDiv = { op: 'div', op1: Number(op1), op2: Number(op2), result: Number(op1) / Number(op2) }
+          res.json(resultDiv)
+        } catch (e) {
+          res.send(e.message)
+        }
+        break
+      case 'mod':
+        try {
+          const resultMod = { op: 'mod', op1: Number(op1), op2: Number(op2), result: Number(op1) % Number(op2) }
+          res.json(resultMod)
+        } catch (e) {
+          res.send(e.message)
+        }
+        break
+      default:
+        res.send('Unknown operator') //Message d'erreur si l'operateur n'est pas bon
+        break
+    }
+  }
+})
+
+app.listen(PORT, IP_LOOPBACK, () => {
+  console.log(`App listening at http://localhost:${PORT}`)
+})
---
+++ web-info.js
@@ -0,0 +1,39 @@
+const jsdom = require("jsdom")
+const { JSDOM } = jsdom
+const axios = require('axios')
+
+const main = async () => {
+  if (process.argv.length !== 3) {
+    console.log('Usage: node my-wget.js "url"')
+    process.exit(1)
+  } else {
+    try {
+      const response = await axios.get(`${process.argv[2]}`)
+      const dom = new JSDOM(response.data)
+
+      const urlTag = process.argv[2]
+      const titleTag = dom.window.document.querySelector('title').textContent
+      //console.log(`title: ${titleTag}`)
+      const links = dom.window.document.querySelectorAll('a').length
+      console.log(links)
+      const nbImgs = dom.window.document.querySelectorAll('img')
+      //console.log(nbImgs.length)
+      const contentLength = response.headers['content-length']
+      //console.log(contentLength)
+      const info = {
+        url: urlTag,
+        contentLength: contentLength,
+        title: titleTag,
+        nbUrls: links,
+        nbImgs: nbImgs
+      }
+
+    } catch (e) {
+      console.log(e.message)
+    }
+  }
+}
+
+main()
+
+//Malheureusement pas terminé...
